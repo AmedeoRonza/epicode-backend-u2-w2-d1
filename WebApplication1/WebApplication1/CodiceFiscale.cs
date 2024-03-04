3869bf49471089281863a486c6fb6234e4a3f1b2
Amedeo Ronza <amedeoronza@gmail.com>
2024-03-04 17:03:26 +0100

    Aggiungere i file di progetto.

---
+++ WebApplication1/WebApplication1/CodiceFiscale.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+
+namespace GestionaleCorriere
+{
+    public class CodiceFiscale : ValidationAttribute
+
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            string codiceFiscale = value.ToString();
+            if (codiceFiscale.Length != 16)
+            {
+                return new ValidationResult("Il codice fiscale deve essere lungo 16 caratteri");
+            }
+            return ValidationResult.Success;
+        }
+
+
+        //public class CodiceFiscaleValidator
+        //{
+        //    public static bool IsValidCodiceFiscale(string codiceFiscale)
+        //    {
+        //        // Espressione regolare per il codice fiscale italiano
+        //        string pattern = @"^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$";
+
+        //        // Regex per la convalida
+        //        Regex regex = new Regex(pattern);
+
+        //        // Verifica se il codice fiscale corrisponde al pattern
+        //        return regex.IsMatch(codiceFiscale);
+        //    }
+
+            
+        //}
+
+    }
+}
---
+++ WebApplication1/WebApplication1/Controllers/ClienteController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult InsertCliente() 
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult InsertCliente(Cliente Model) 
+        {
+            string connectionString = ConfigurationManager.ConnectionStrings["GestionaleSpedizioni"].ConnectionString.ToString();
+            SqlConnection conn = new SqlConnection(connectionString);
+            conn.Open();
+            try
+            {
+
+                string query = "INSERT INTO Spedizioni (idCliente, Nominativo, IsAzienda, codiceFiscale, partitaIva) VALUES ( @idCliente, @Nominativo, @IsAzienda, @codiceFiscale, @partitaIva )";
+
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@idCliente", Model.idCliente);
+                cmd.Parameters.AddWithValue("@Nominativo", Model.Nominativo);
+                cmd.Parameters.AddWithValue("@IsAzienda", Model.IsAzienda);
+                cmd.Parameters.AddWithValue("@codiceFiscale", Model.codiceFiscale);
+                cmd.Parameters.AddWithValue("@partitaIva", Model.partitaIva);
+
+                cmd.ExecuteNonQuery();
+            }
+            catch (SqlException ex)
+            {
+                System.Diagnostics.Debug.WriteLine("Errore nella richiesta SQL");
+                return View(ex.Message);
+            }
+            finally
+            {
+                conn.Close();
+            }
+
+            return View();
+        }
+    }
+}
